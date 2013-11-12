7953c65e483effbcb5e0a5d5d09586ed0db0c067
Dylan Jay <software@pretaweb.com>
2013-11-12 16:15:55 +1100

    whitelist zipfile and csv modules

--- pretaweb/plominolib/__init__.py
+++ pretaweb/plominolib/__init__.py
@@ -1,6 +1,7 @@
 """
 Make tkt auth from Plone Session available to PlominoUtils.
 """
+from AccessControl import allow_class, ModuleSecurityInfo
 from zope import component
 from Products.CMFPlomino import interfaces
 from zope.interface import implements
@@ -8,6 +9,7 @@ from zope.interface import implements
 import time
 import base64
 from plone.session import tktauth
+from zipfile import ZipFile as _ZipFile, ZIP_STORED, ZipInfo
 
 from Products.PythonScripts.Utility import allow_module
 
@@ -15,6 +17,17 @@ allow_module('pretaweb.plominolib')
 # allow_module('time')
 # allow_module('tktauth')
 
+#Uses files but doesn't open them. need to double check
+allow_module('csv')
+# ModuleSecurityInfo('nntplib').declarePublic('NNTP',
+#   'error_reply', 'error_temp', 'error_perm', 'error_proto')
+from csv import DictReader, DictWriter, Dialect, excel, excel_tab, Sniffer
+allow_class(DictReader)
+allow_class(DictWriter)
+allow_class(Dialect)
+allow_class(excel)
+allow_class(excel_tab)
+allow_class(Sniffer)
 
 def encode(secret_key, email):
     """
@@ -72,6 +85,45 @@ class PretawebPlominoLibUtils:
 component.provideUtility(PretawebPlominoLibUtils, interfaces.IPlominoUtils,
                          name='pretaweb.plominolib')
 
+
+class SafeZipFile(_ZipFile):
+    """ SafeZipFile
+    """
+    def __init__(self, file, mode="r", compression=ZIP_STORED, allowZip64=False):
+        if not isinstance(file, basestring):
+            return _ZipFile.__init__(self, file, mode, compression, allowZip64)
+        else:
+            raise NotImplemented("Paths not supported by SafeZipFile")
+
+    def extract(self, member, path=None, pwd=None):
+        """Extract a member from the archive to the current working directory,
+           using its full name. Its file information is extracted as accurately
+           as possible. `member' may be a filename or a ZipInfo object. You can
+           specify a different directory using `path'.
+        """
+        raise NotImplemented("Paths not supported by SafeZipFile")
+
+    def extractall(self, path=None, members=None, pwd=None):
+        """Extract all members from the archive to the current working
+           directory. `path' specifies a different directory to extract to.
+           `members' is optional and must be a subset of the list returned
+           by namelist().
+        """
+        raise NotImplemented("Paths not supported by SafeZipFile")
+
+    def write(self, filename, arcname=None, compress_type=None):
+        """Put the bytes from filename into the archive under the name
+        arcname."""
+        raise NotImplemented("Paths not supported by SafeZipFile")
+
+allow_class(SafeZipFile)
+ModuleSecurityInfo('zipfile').declarePublic('ZIP64_LIMIT',
+                                             'ZIP_FILECOUNT_LIMIT',
+                                             'ZIP_MAX_COMMENT',
+                                             'ZIP_STORED',
+                                             'ZIP_DEFLATED',
+                                             'ZipInfo')
+allow_class(ZipInfo)
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
