04a746b25419915eaa1e68e44ec756255ca07264
Ivan Teoh <ivan.teoh@gmail.com>
2016-06-07 16:57:48 +1000

    Add pdf convert

--- pretaweb/plominolib/__init__.py
+++ pretaweb/plominolib/__init__.py
@@ -329,9 +329,67 @@ def initialize(context):
     """Initializer called when used as a Zope 2 product."""
 
 from plone import api
+from plone.subrequest import subrequest
+from StringIO import StringIO
+from urllib import unquote
+from xhtml2pdf.document import pisaDocument
+from zope.contenttype import guess_content_type
+from zope.contenttype import text_type
+
+import urlparse
 
 
-def apply_diazo(html, as_xml=False):
+def fetch_resources(uri, rel):
+    """Callback to allow pisa/reportlab to retrieve Images,Stylesheets, etc.
+
+    `uri` is the href attribute from the html link element.
+    `rel` gives a relative path, but it's not used here.
+    """
+    site = getSite()
+    urltool = getToolByName(site, "portal_url")
+    portal = urltool.getPortalObject()
+    base = portal.absolute_url()
+    uri = urlparse.urljoin(rel, uri)
+    if uri.startswith(base):
+        uri = uri[len(base) + 1:]
+    response = subrequest(unquote(uri))
+    if response.status == 301:
+        new_uri = response.headers['location']
+        response = subrequest(unquote(new_uri))
+    if response.status != 200:
+        return None
+
+    content_type = response.getHeader('content-type')
+    if content_type:
+        try:
+            # stupid pisa doesn't let me send charset.
+            ctype, encoding = response.getHeader(
+                'content-type').split('charset=')
+            ctype = ctype.split(';')[0]
+        except ValueError:
+            ctype = response.getHeader('content-type').split(';')[0]
+            encoding = 'utf8'
+    else:
+        # content-type in headers could be empty,
+        # we need to use guess_content_type and text_type
+        # to guess the content-type
+        ctype, encoding = guess_content_type(uri)
+        if ctype and ctype.startswith('text/'):
+            ctype = text_type(uri)
+
+    if ctype == 'text/css':
+        # pisa only likes ascii css
+        # in order to backward compatible old version, don't put keywords
+        # encode in version 2.7: Support for keyword arguments added
+        data = response.getBody().decode(encoding).encode('ascii', 'ignore')
+    else:
+        data = response.getBody()
+    data = data.encode("base64").replace("\n", "")
+    data_uri = 'data:{0};base64,{1}'.format(ctype, data)
+    return data_uri
+
+
+def apply_diazo(html, content_type="html"):
     request = getRequest()
     portal = api.portal.get()
     charset = portal.portal_properties.site_properties.default_charset
@@ -346,13 +404,29 @@ def apply_diazo(html, as_xml=False):
         new_html = theme_handler.transformIterable([html], charset)
     # If the theme is not enabled, transform returns None
     if new_html is not None:
-        if as_xml:
+        if content_type == "xml":
             new_html.tree = etree.ElementTree(
                 new_html.tree.xpath('/html/body/*')[0])
             new_html.doctype = '<?xml version="1.0" standalone="yes" ?>'
         new_html = new_html.serialize()
     else:
         new_html = html
+
+    # path=self.context.absolute_url(),
+    if content_type == "pdf":
+        pdf = StringIO()
+        pisaDocument(
+            new_html,
+            pdf,
+
+            raise_exception=True,
+            link_callback=fetch_resources)
+        # pisadoc = pisaDocument(html, pdf, raise_exception=True)
+        assert pdf.len != 0, 'Pisa PDF generation returned empty PDF!'
+        # html.close()
+        new_html = pdf.getvalue()
+        pdf.close()
+
     return new_html
 
 
